9027b4e30586a7207072cbb78db66aa82d4124e0
Skadi_the_Wolf <yawaluke6@gmail.com>
2025-08-12 16:56:37 +0200

    changed UrbanObserver.py

--- FaustBot/Modules/UrbanObserver.py
+++ FaustBot/Modules/UrbanObserver.py
@@ -1,5 +1,5 @@
 from FaustBot.Modules.PrivMsgObserverPrototype import PrivMsgObserverPrototype
-
+import requests
 
 class UrbanObserver(PrivMsgObserverPrototype):
     @staticmethod
@@ -11,30 +11,41 @@ class UrbanObserver(PrivMsgObserverPrototype):
         return ".urban <term> - fragt Urban Dictionary zu <term> ab"
 
     def update_on_priv_msg(self, data, connection):
-
         if data['message'].find('.urban') == -1:
             return
-        w = wikipedia.set_lang('de')
-        q = data['message'].split(' ')
-        query = ''
-        for word in q:
-            if word.strip() != '.w':
-                query += word + ' '
-        w = wikipedia.search(query)
-        if w.__len__() == 0:
-            connection.send_back(data['nick'] + ', ' +
-                                'ich habe dazu keinen eintrag gefunden!',
-                                 data)
-            return
-        try:
-            page = wikipedia.WikipediaPage(w.pop(0))
-        except wikipedia.DisambiguationError as error:
-            print('disambiguation page')
-            page = wikipedia.WikipediaPage(error.args[1][0])
-        connection.send_back(data['nick'] + ' ' + page.url, data)
-        index = 51 + page.summary[50:350].rfind('. ')
-        if index == 50 or index > 230:
-            index = page.summary[0:350].rfind(' ')
-            connection.send_back(page.summary[0:index], data)
-        else:
-            connection.send_back(page.summary[0:index], data)
+
+        if data['message'].startswith('.urban'):
+
+            arr = data['message'].split(' ',1)
+
+            if arr[0] == ".urban":
+                search = arr[1]
+
+                contents = requests.get(
+                    f'https://unofficialurbandictionaryapi.com/api/search?term={search}&limit=1&page=1&')
+
+                contentStr = str(contents.content)
+                status = int(contents.status_code)
+                # print(contentStr)
+                # print(status)
+
+                if status == 200:
+
+                    indexWord = contentStr.index('"word"')
+                    indexWordEnd = contentStr.index('","meaning"')
+                    indexMeaning = contentStr.index('"meaning"')
+                    indexMeaningEnd = contentStr.index('","example"')
+
+                    WordStr = contentStr[indexWord:indexWordEnd]
+                    MeaningStr = contentStr[indexMeaning:indexMeaningEnd]
+                    WordSplit = WordStr.split('":"')
+                    MeaningSplit = MeaningStr.split('":"')
+
+                    connection.send_back(self, f'Wort: {WordSplit}')
+                    connection.send_back(self, f'Meaning: {MeaningSplit}')
+
+
+
+
+
+
