24ea2679e7ec459501c742fedfcfa81369901a3a
SkadiTheWolf <147843853+SkadiTheWolf@users.noreply.github.com>
2025-09-24 22:31:21 +0200

    removed unnecessary code

--- FaustBot/Modules/UrbanObserver.py
+++ FaustBot/Modules/UrbanObserver.py
@@ -14,7 +14,7 @@ class UrbanObserver(PrivMsgObserverPrototype):
 
     def update_on_priv_msg(self, data, connection):
 
-        if data['message'].find('.urban') == -1:
+        if data['messageCaseSensitive'].find('.urban') == -1:
             return
 
         #split incoming message in '.urban' and '<term>'
@@ -45,32 +45,3 @@ class UrbanObserver(PrivMsgObserverPrototype):
 
             connection.send_back(f'{data['nick']} Das gesuchte Wort {WordSplit[1]} hat folgende Bedeutung:',data)
             connection.send_back(MeaningSplit[1], data)
-
-
-
-
-
-        '''w = wikipedia.set_lang('de')
-        q = data['message'].split(' ')
-        query = ''
-        for word in q:
-            if word.strip() != '.w':
-                query += word + ' '
-        w = wikipedia.search(query)
-        if w.__len__() == 0:
-            connection.send_back(data['nick'] + ', ' +
-                                'ich habe dazu keinen eintrag gefunden!',
-                                 data)
-            return
-        try:
-            page = wikipedia.WikipediaPage(w.pop(0))
-        except wikipedia.DisambiguationError as error:
-            print('disambiguation page')
-            page = wikipedia.WikipediaPage(error.args[1][0])
-        connection.send_back(data['nick'] + ' ' + page.url, data)
-        index = 51 + page.summary[50:350].rfind('. ')
-        if index == 50 or index > 230:
-            index = page.summary[0:350].rfind(' ')
-            connection.send_back(page.summary[0:index], data)
-        else:
-            connection.send_back(page.summary[0:index], data)'''
