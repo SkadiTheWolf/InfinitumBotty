3ea2ed218ec219433532201da3364afcdd684183
SkadiTheWolf <yawaluke6@gmail.com>
2025-09-25 23:16:50 +0200

    added themen

--- thema.py
+++ thema.py
@@ -1,1 +1,4 @@
-thema = ['Grönland', 'Basketball', 'Handball', 'den Tatort "Die Bielefeld-Verschwörung']
+thema = ['Grönland', 'Basketball', 'Handball', 'den Tatort "Die Bielefeld-Verschwörung',
+         'obskure Musik', 'obskure Filme', 'den einen Film, Dings, der handelt von dem Teil, und dem Mann',
+         'unsere Lieblingsnussmischung', 'unsere Lieblingshunderunde', 'unser Lieblingssport',
+         'das Betriebssystem das wir benutzen']
