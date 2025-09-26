45109eb85390d007928d26af955c1aad3175433f
SkadiTheWolf <yawaluke6@gmail.com>
2025-09-26 15:25:08 +0200

    typo

--- thema.py
+++ thema.py
@@ -1,7 +1,7 @@
 thema = ['Grönland', 'Basketball', 'Handball', 'den Tatort "Die Bielefeld-Verschwörung',
          'obskure Musik', 'obskure Filme', 'den einen Film, Dings, der handelt von dem Teil, und dem Mann',
          'unsere Lieblingsnussmischung', 'unsere Lieblingshunderunde', 'unser Lieblingssport',
-         'das Betriebssystem das wir benutzen', 'Fussball', 'Crocket', 'Langlauf', 'Ski-Slalom',
+         'das Betriebssystem das wir benutzen', 'Fußball', 'Crocket', 'Langlauf', 'Ski-Slalom',
          'verschiedene Hunderassen', 'Hockey', 'Eishockey', 'Die Antarktis', 'die Arktis', 'Kinderhörspiele',
          'Rezeptideen', 'nichts', 'gar nichts', 'nichts, gähnende Leere', 'nordische Mythologie', 'Fridolin den Fuchs',
          'Füchse', 'Wölfe', 'Eulen', 'Autismus', 'dem Wetter gerade', 'normale Hobbies', 'seltsame Hobbies',
