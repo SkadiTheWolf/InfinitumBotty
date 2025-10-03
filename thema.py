edd7df7710b7e3b1dc9de952d744a7081711d64d
SkadiTheWolf <147843853+SkadiTheWolf@users.noreply.github.com>
2025-10-03 20:24:17 +0200

    deleted unneeded themen.py
    deleted unneeded themen.py

--- thema.py
+++
@@ -1,9 +0,0 @@
-thema = ['Grönland', 'Basketball', 'Handball', 'den Tatort "Die Bielefeld-Verschwörung',
-         'obskure Musik', 'obskure Filme', 'den einen Film, Dings, der handelt von dem Teil, und dem Mann',
-         'unsere Lieblingsnussmischung', 'unsere Lieblingshunderunde', 'unser Lieblingssport',
-         'das Betriebssystem das wir benutzen', 'Fußball', 'Crocket', 'Langlauf', 'Ski-Slalom',
-         'verschiedene Hunderassen', 'Hockey', 'Eishockey', 'Die Antarktis', 'die Arktis', 'Kinderhörspiele',
-         'Rezeptideen', 'nichts', 'gar nichts', 'nichts, gähnende Leere', 'nordische Mythologie', 'Fridolin den Fuchs',
-         'Füchse', 'Wölfe', 'Eulen', 'Autismus', 'dem Wetter gerade', 'normale Hobbies', 'seltsame Hobbies',
-         'Gefühle', 'Züge', 'Busse', 'Emacs', 'Biologie', 'Chemie', 'Mathe', 'die deutsche Sprache',
-         'die englische Sprache', 'die spanische Sprache', 'Schottergärten', 'Piraten', 'Seefahrer', 'Mäuse']
