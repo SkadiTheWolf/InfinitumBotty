4f3145ee691b934b8c51bd1e5e63f48f30fe7f73
SkadiTheWolf <yawaluke6@gmail.com>
2026-02-02 12:29:05 +0100

    Rewrote BaseObserver.py, Now Functional, testing needed

--- FaustBot/Modules/BaseObserver.py
+++ FaustBot/Modules/BaseObserver.py
@@ -5,75 +5,86 @@ This module calculates a decimal to binary and back
 January 2026, Skadi Wiesemann
 
 """
-from unicodedata import digit
 
 from FaustBot.Communication.Connection import Connection
 from FaustBot.Modules.PrivMsgObserverPrototype import PrivMsgObserverPrototype
 
+
 def check_if_int(zahl):
+    """
+    Checking if the different inputs are an Integer
+    """
     try:
         int(zahl)
     except ValueError:
         return ValueError
 
+
 def check_if_base(base, zahl):
-    digit = 0
+    """
+    Checking if the Number entered is a BaseX number by taking the different digits and comparing it to X-1
+    """
     while zahl != 0:
         digit = zahl % 10
         if digit >= base:
             return False
-        digit //= zahl
+        zahl //= 10
     return True
 
+
 def check_if_bigger(von, zu):
+    """
+    Check wich Base is bigger
+    """
     if von > zu:
         return True
     else:
         return False
 
+
 def decimal_to_base(base, zahl):
     out = ''
+    zahl = int(zahl)
     while zahl > 0:
-        out = str(zahl % base) + out
+
+        out = str(zahl% base) + out
         zahl //= base
     return out
 
-def base_to_decimal(base, zahl):
-    out = ''
 
-    digit, position = 0,0
+def base_to_decimal(base, zahl):
+
+    digit, position = 0, 0
     while zahl:
-        digit += (zahl % 10)*(2**position)
+        digit += (zahl % 10) * (base ** position)
         zahl //= 10
-        position +=1
+        position += 1
     out = str(digit)
     return out
 
 
-def small_to_big(von, zu, zahl):
-    out = 'not implemented yet'
+def base_to_base(von, zu, zahl):
+
+    out = decimal_to_base(zu, base_to_decimal(von, zahl))
 
     return out
 
-def big_to_small(von, zu, zahl):
-    out = 'not implemented yet'
-
-    return out
 
 class BaseObserver(PrivMsgObserverPrototype):
+
     @staticmethod
     def cmd():
-        return [".base"]
+        return ['.base']
 
     @staticmethod
     def help():
-        return '.base <von> <zu> <Zahl> - Rechnet eine Zahl in eine andere Base um'
+        return ".base <von> <zu> <Zahl> - Rechnet eine Zahl in eine andere Base um"
 
     def update_on_priv_msg(self, data: dict, connection: Connection):
 
-        if data['message'].startswith(".base"):
+        if data['message'].startswith('.base'):
 
-            array = data['messageCaseSensitive'].split(' ', 3)
+            array = data['message'].split(' ', 3)
 
             zahlString = array[3]
             err = check_if_int(zahlString)
@@ -112,7 +123,7 @@ class BaseObserver(PrivMsgObserverPrototype):
                 connection.send_back(f'Die Zahl {zahlString} ist keine Base{vonString} Zahl', data)
                 return
 
-            #if zahl > 1000000:
+            if zahl > 1000000:
                 connection.send_back(f'Zahl zu Groß', data)
                 return
 
@@ -124,42 +135,12 @@ class BaseObserver(PrivMsgObserverPrototype):
                 out = decimal_to_base(zu, zahl)
 
             elif bigger:
-                out = big_to_small(von, zu, zahl)
+                out = base_to_base(von, zu, zahl)
 
             elif not bigger:
-                out = small_to_big(von, zu, zahl)
+                out = base_to_base(von, zu, zahl)
 
             else:
                 connection.send_back('Unbekannter Fehler', data)
 
             connection.send_back(f'Die Zahl {zahlString} entspricht {out}', data)
-
-
-
-
-            print(array)
-
-
-
-
-
-'''
-            match base:
-                case '2':
-                    try:
-                        zahlDezimal = int(zahlString)
-                        output = '{0:b}'.format(zahlDezimal)
-                        connection.send_back(f'Die Zahl {zahlString} entspricht {output}', data)
-                    except ValueError:
-                        connection.send_back('Fehler: Keine Ganzzahl', data)
-
-                case '10':
-                    try:
-                        output = int(zahlString, 2)
-                        connection.send_back(f'Die Zahl {zahlString} entspricht {output}', data)
-                    except ValueError:
-                        connection.send_back('Fehler: Keine Zahl', data)
-
-                case _:
-                    connection.send_back(f'Base {base} nicht verfügbar', data)
-'''
