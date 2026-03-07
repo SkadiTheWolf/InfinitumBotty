b5333a3f6e50d9f0117e2110a936ad880399c5a9
Context 77 <126421199+ctx77@users.noreply.github.com>
2026-03-07 14:36:07 +0100

    improve tea and some autoformatting

--- AdaptHanWordsFromLegacy.py
+++ AdaptHanWordsFromLegacy.py
@@ -3,16 +3,17 @@
 
 from FaustBot.Model.HanDatabaseProvider import HanDatabaseProvider
 import csv
+
 HanDBProvider = HanDatabaseProvider()
 wordList = open("HangmanLog")
-wordListWords = csv.reader(wordList, delimiter=';', quotechar='|')
+wordListWords = csv.reader(wordList, delimiter=";", quotechar="|")
 randomChoicePool = []
 for word in wordListWords:
     print(word)
     print(word[1].strip())
     no = False
-    for char in  ['ä','ü','ö','ß']:
-       if char.upper() in word[1].strip().upper():
+    for char in ["ä", "ü", "ö", "ß"]:
+        if char.upper() in word[1].strip().upper():
             no = True
     if not no:
         HanDBProvider.addWord(word[1].strip())
